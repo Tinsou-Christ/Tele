5598f64c7f595ef0d3a415433345bc21c886c305
Tinsou-Christ <anosvoldigo291@gmail.com>
2026-07-03 11:47:05 +0000

    Modify connectMongoDB to include dbName parameter
    Updated connectMongoDB function to accept dbName parameter and removed deprecated mongoose options.

--- database/connectDB/connectMongoDB.js
+++ database/connectDB/connectMongoDB.js
@@ -1,15 +1,18 @@
-module.exports = async function (uriConnect) {
+// database/connectDB/connectMongoDB.js
+module.exports = async function (uriConnect, dbName) {
 	const mongoose = require("mongoose");
+	mongoose.set("strictQuery", false);
 
+	// ⚠️ Ces require pointaient vers "../models/mongodb/*.js" qui n'existait pas.
+	// Les modèles sont maintenant bien présents dans database/models/mongodb/
 	const threadModel = require("../models/mongodb/thread.js");
 	const userModel = require("../models/mongodb/user.js");
 	const dashBoardModel = require("../models/mongodb/userDashBoard.js");
 	const globalModel = require("../models/mongodb/global.js");
 
-	await mongoose.connect(uriConnect, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	});
+	// Les options useNewUrlParser / useUnifiedTopology sont dépréciées
+	// et n'existent plus dans les versions récentes de mongoose (>=6).
+	await mongoose.connect(uriConnect, dbName ? { dbName } : undefined);
 
 	return {
 		threadModel,
